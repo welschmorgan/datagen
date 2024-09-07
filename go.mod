5b6cba10a98b20d3478cd38225742c024632660a
Morgan Welsch <welschmorgan@gmail.com>
2024-09-07 21:16:23 +0200

    Improve BasicUploader progress display

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.23.0
 
 require (
 	github.com/MatusOllah/slogcolor v1.2.2
-	github.com/fatih/color v1.16.0
+	github.com/fatih/color v1.17.0
 	github.com/kirsle/configdir v0.0.0-20170128060238-e45d2f54772f
 	github.com/mattn/go-sqlite3 v1.14.23
 	golang.org/x/text v0.18.0
@@ -13,6 +13,5 @@ require (
 require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/rs/zerolog v1.33.0 // indirect
-	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 )
--- pkg/seed/upload.go
+++ pkg/seed/upload.go
@@ -4,7 +4,10 @@ import (
 	"database/sql"
 	"fmt"
 	"log/slog"
+	"os"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/welschmorgan/datagen/pkg/models"
 )
@@ -48,8 +51,40 @@ func (u *BasicUploader) Upload(data []ParserRow) error {
 	if err != nil {
 		return fmt.Errorf("failed to prepare insert query, %s", err)
 	}
+
+	sizeOfData := fmt.Sprint(len(data))
+	var batchSize int64
+	var timeStart = time.Now()
+	var lastBatchStart time.Time = timeStart
+	var lastBatchTime time.Duration
+	batchSize, err = strconv.ParseInt(sizeOfData[0:len(sizeOfData)-1], 10, 64)
+	if err != nil {
+		return err
+	}
+	numRows := int64(len(data))
+	numBatchesTotal := numRows / batchSize
+	numBatchesLeft := numBatchesTotal
+
 	numInserted := 0
-	for i, item := range data {
+	// rowStartTime := time.Now()
+	// var timePerRow time.Duration = 0
+	var i int
+	var item ParserRow
+	print("\x1b[?25l")
+	defer func() { print("\x1b[?25h") }()
+	for i, item = range data {
+		if int64(i)%batchSize == 0 {
+			eta := time.Second * time.Duration(float64(numBatchesLeft)*lastBatchTime.Seconds())
+			progress_value := float64(i) / float64(len(data))
+			numBars := 10
+			numProgressBars := int(progress_value * float64(numBars))
+			progress := fmt.Sprintf("%s%s", strings.Repeat("▉", numProgressBars), strings.Repeat(" ", numBars-numProgressBars))
+			fmt.Fprintf(os.Stderr, "\r\x1b[2K[%s] uploading '\x1b[0;1m%s\x1b[0m' [\x1b[0;34m%s\x1b[0m per batch (%d), %d batches left, ETA: \x1b[0;33m%s\x1b[0m]\r", progress, item.value, lastBatchTime, batchSize, numBatchesLeft, eta)
+			now := time.Now()
+			lastBatchTime = now.Sub(lastBatchStart)
+			lastBatchStart = now
+			numBatchesLeft = (numRows - int64(i)) / batchSize
+		}
 		if !propExists(item.value) {
 			_, err := stmt.Exec(item.locale.Id, u.typ, item.value)
 			if err != nil {
@@ -57,8 +92,10 @@ func (u *BasicUploader) Upload(data []ParserRow) error {
 			}
 			numInserted += 1
 		}
+		// timePerRow = time.Since(rowStartTime)
 	}
-	slog.Debug(fmt.Sprintf("Inserted %d/%d fresh values", numInserted, len(data)))
+	println()
+	slog.Debug(fmt.Sprintf("Inserted %d/%d fresh values in %s", numInserted, len(data), time.Since(timeStart)))
 	return tx.Commit()
 }
 
